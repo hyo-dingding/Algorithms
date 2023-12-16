801bd67d1c247186b460ca7086b6fa4539e9add6
hyo-dingding <107694757+hyo-dingding@users.noreply.github.com>
2023-12-16 14:53:51 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
@@ -1,13 +1,8 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> Int {
-       var num = [0,1,2,3,4,5,6,7,8,9]
-    var count = 0
+ var num = 45
+    var result = numbers.reduce(0, +)
     
-    for i in num {
-        if !numbers.contains(i) {
-            count += i
-        }
-    }
-    return count
+    return num - result
 }
